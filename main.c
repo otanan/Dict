04fc9615a731255cc4e3964c4f7910deb0305a1d
Jonathan Delgado <jonathandelgado0327@gmail.com>
2018-12-27 14:17:08 -0500

    Updated main.c to reflect testing

--- main.c
+++ main.c
@@ -1,39 +1,33 @@
 #include <stdio.h>
+#include <string.h>
+
 #include "dict.h"
 //Need to fix logic of pointers or pointers of pointers
 //Consider hashing?
 //Organize functions
 //Implement type checking with generics
 //Consider implementing memcpy in values as well, instead of just keys
+//Check malloc/realloc calls
 
-
-void printDict(Dict *self) {
-	if(self->isEmpty(self)) {
-		printf("Empty dictionary\n");
-		return;
-	}
-
-	printf("[");
-
-	for(int i = 0; i < self->length(self); i++) {
-		void *key = self->getKey(self, i);
-		//Temporary, already assuming that the keys are strings
-		//and the values are integers
-		printf("(%s, %d), ", key, *(int *)self->get(self, key));
-	}
-
-	printf("\b\b]\n");
+static void key_to_string(void *key, char *string);
+static void key_to_string(void *key, char *string) {
+	int len = strlen((char *) key) + 1;
+    memcpy(string, key, len);
 }
 
-int main() {
 
+int main() {
 	Dict *dict = newDict();
+	dict->set_key_to_string(dict, key_to_string);
+
 	char *label = "first";
 	char *label2 = "second";
 	char *label3 = "third";
 	char *label4 = "fourth";
 	char *label5 = "fifth";
-	
+	char *label6 = "ACCESSED WRONG";
+
+
 	int x = 10;
 	int y = 2;
 	int z = -1;
@@ -44,6 +38,11 @@ int main() {
 	dict->set(dict, label3, &z);
 	dict->set(dict, label4, &a);
 	dict->set(dict, label5, &b);
+	dict->set(dict, label6, &x);
+	//
+	//
+	
+
 	dict->print(dict);
 
 
